883b9eac6523daddf2d4aca47f58598cd3399cc6
Kathy <65121394+kfahn22@users.noreply.github.com>
2022-11-22 11:17:09 -0500

    Move not state variables to different files

---
+++ js/health.js
@@ -0,0 +1,22 @@
+const HEALTH = {
+    good: {
+        value: 4,
+        name: 'Good',
+    },
+    adequate: {
+        value: 3,
+        name: 'Adequate',
+    },
+    poor: {
+        value: 2,
+        name: 'Poor',
+    },
+    verypoor: {
+        value: 1,
+        name: 'Very poor',
+    },
+    dead: {
+        value: 0,
+        name: 'Asleep forever', // (dead is not nice)
+    },
+};
---
+++ js/rations.js
@@ -0,0 +1,14 @@
+const RATIONS = {
+    filling: {
+      value: 3,  // amount of food consumed per person per day
+      name: 'Filling', 
+    },
+    meager: {
+     value: 2,
+     name: 'Meager',
+   },
+   barebones: {
+     value: 1,
+     name: 'Bare Bones',
+   },
+ };
